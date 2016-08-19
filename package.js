4dc8f06acb0d3920e4bd0d03159b21b938518da8
yoyohoo <sinful_001@sina.com>
2016-08-19 17:56:16 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,27 @@
+{
+  "name": "ocApp",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "grunt": "^1.0.1",
+    "grunt-contrib-clean": "^1.0.0",
+    "grunt-karma": "^2.0.0",
+    //"grunt-karma-sonar": "^0.2.26",
+    "jasmine-core": "^2.4.1",
+    "karma": "^0.13.22",
+    "karma-chrome-launcher": "^1.0.1",
+    "karma-coverage": "^1.1.0",
+    "karma-jasmine": "^1.0.2",
+    "karma-junit-reporter": "^1.1.0",
+    "karma-junit-sonarqube-reporter": "0.0.1",
+    "karma-phantomjs-launcher": "^1.0.1",
+    "karma-requirejs": "^1.0.0"
+  }
+}
